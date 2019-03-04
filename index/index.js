2f2e7312f2896129ffdb37f570c5a5f5b7ba80eb
zhangyufan <zhangyufanzyf@qq.com>
2019-03-04 20:00:51 +0800

    new SelfVue的时候做一个代理处理

--- index/index.js
+++ index/index.js
@@ -6,11 +6,35 @@
  *  exp -> name(key)
  **/
 function SelfVue (data, el, exp) {
+    var self = this;
     this.data = data;
+
+    Object.keys(data).forEach(function(key) {
+        self.proxyKeys(key);  // 绑定代理属性
+    });
+
     observe(data);
     el.innerHTML = this.data[exp];  // 初始化模板数据的值
     new Watcher(this, exp, function (value) {
         el.innerHTML = value;
     });
     return this;
+}
+
+//我们给new SelfVue的时候做一个代理处理，让访问selfVue的属性代理为访问selfVue.data的属性，
+// 实现原理还是使用Object.defineProperty( )对属性值再包一层
+SelfVue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function proxyGetter() {
+                return self.data[key];
+            },
+            set: function proxySetter(newVal) {
+                self.data[key] = newVal;
+            }
+        });
+    }
 }
--- index/observer.js
+++ index/observer.js
@@ -52,7 +52,8 @@ function observe(data) {
     if (!data || typeof data !== 'object') {
         return;
     }
-    // Object.keys() 方法会返回一个由一个给定对象的自身可枚举属性组成的数组，数组中属性名的排列顺序和使用 for...in 循环遍历该对象时返回的顺序一致 。
+    // Object.keys() 方法会返回一个由一个给定对象的自身可枚举属性组成的数组，
+    // 数组中属性名的排列顺序和使用 for...in 循环遍历该对象时返回的顺序一致 。
     Object.keys(data).forEach(function(key) {
         defineReactive(data, key, data[key]);
     });
